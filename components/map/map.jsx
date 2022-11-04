9907226233d25b3615abb60b77582860dd86ea01
Arin Dewangan <arinaws59@gmail.com>
2022-11-04 21:45:35 +0530

    optimised for mobile

--- components/map/map.jsx
+++ components/map/map.jsx
@@ -5,7 +5,7 @@ import what3words from '@what3words/api'
 import { What3wordsService } from '@what3words/api/dist/service'
 import { drawChosenSquares, drawGrid } from '../../helpers'
 import Header from '../Header'
-import Camera from '../Camera'
+import CameraPopup from '../CameraPopup'
 
 const GREEN = '#1ec716'
 const options = {
@@ -189,18 +189,21 @@ function Map() {
       )}
       <Header words={words} />   
       <div style={{ position: 'absolute', bottom: '1rem', left: '2rem', right: '2rem', fontSize: '13px', padding: '5px', zIndex: 401,}}>
-        <div className="relative w-[50%]">
-          <div className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            {chosenSquares.join(' ')}
+        <div className="relative md:w-[fit-content]">
+          <div className="flex p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <div className='py-2 px-2'>
+              {chosenSquares.join(' ')}
+            </div>
+            <div className='flex flex-row-reverse'>
+              {isClaiming ? (
+                <button onClick={finishTracking} type="button" className="absolute right-2.5 bottom-2.5 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg px-6 py-2">Claim Land</button>
+              ) : (
+                <button onClick={startTracking} type="button" className=" text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-6 py-2">Claim Tile</button>
+              )}
+              <CameraPopup popupOpened={popupOpened} setPopupOpened={setPopupOpened} chosenSquares={chosenSquares} />
+            </div>
           </div>
           <div>
-            {isClaiming ? (
-              <button onClick={finishTracking} type="button" className="absolute right-2.5 bottom-2.5 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg px-6 py-2">Claim Land</button>
-            ) : (
-              <button onClick={startTracking} type="button" className="absolute right-2.5 bottom-2.5 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg px-6 py-2">Claim Tile</button>
-            )}
-            {/* <CameraPopup popupOpened={popupOpened} setPopupOpened={setPopupOpened} chosenSquares={chosenSquares} /> */}
-            <Camera popupOpened={popupOpened} setPopupOpened={setPopupOpened} chosenSquares={chosenSquares} />
           </div>
         </div>
       </div>
