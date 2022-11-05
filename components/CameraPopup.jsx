314bebe804d2617390d3f03e4e4c102c38cf54a5
Arin Dewangan <arinaws59@gmail.com>
2022-11-05 21:20:57 +0530

    fixed negative value

--- components/CameraPopup.jsx
+++ components/CameraPopup.jsx
@@ -89,7 +89,7 @@ function CameraPopup({ popupOpened, setPopupOpened, chosenSquares, user }) {
                             <h1 className="pl-2 pr-10 py-2">Rate:</h1>
                             <label htmlFor="UserEmail" className="relative block overflow-hidden rounded-md border border-gray-200 px-3 pt-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600">
                               <input 
-                              value={price === 0 ? '' : price}
+                              value={price  <= 0 ? '' : price}
                               onChange={(e) => {
                                 if (!isNaN(+e.target.value)) {
                                     setPrice(+e.target.value)
