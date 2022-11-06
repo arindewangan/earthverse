70c4394bc71238a0c48f2ebad1f7a57283e6bdb5
Arin Dewangan <arinaws59@gmail.com>
2022-11-06 20:12:44 +0530

    added walletconnect

---
+++ pages/signin.jsx
@@ -0,0 +1,56 @@
+import { signIn } from 'next-auth/react';
+import { useAccount, useConnect, useSignMessage, useDisconnect } from 'wagmi';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+
+function SignIn() {
+    const { connectAsync } = useConnect();
+    const { disconnectAsync } = useDisconnect();
+    const { isConnected } = useAccount();
+    const { signMessageAsync } = useSignMessage();
+    const { push } = useRouter();
+
+    const handleAuth = async () => {
+        if (isConnected) {
+            await disconnectAsync();
+        }
+        // added WalletConnectConnector
+        const { account, chain } = await connectAsync({
+            connector: new WalletConnectConnector({
+                options: {
+                    qrcode: true,
+                },
+            }),
+        });
+
+        const userData = { address: account, chain: chain.id, network: 'evm' };
+
+        const { data } = await axios.post('/api/auth/request-message', userData, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        const message = data.message;
+
+        const signature = await signMessageAsync({ message });
+
+        // redirect user after success authentication to '/user' page
+        const { url } = await signIn('credentials', { message, signature, redirect: false, callbackUrl: '/user' });
+        /**
+         * instead of using signIn(..., redirect: "/user")
+         * we get the url from callback and push it to the router to avoid page refreshing
+         */
+        push(url);
+    };
+
+    return (
+        <div>
+            <h3>Web3 Authentication</h3>
+            <button onClick={() => handleAuth()}>Authenticate via WalletConnect</button>
+        </div>
+    );
+}
+
+export default SignIn;
