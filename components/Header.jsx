7f6dac3ebd50c418ac44dbc8d15e5f96cdd14d3d
Arin Dewangan <arinaws59@gmail.com>
2022-11-06 21:17:48 +0530

    fixed bug

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,7 @@ import { useAccount, useConnect, useSignMessage, useDisconnect } from 'wagmi';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import { useSession , signIn, signOut } from "next-auth/react"
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 
 
 function Header({words}) {
